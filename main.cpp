c2deee52a601a8d71d0d74488fb76f10b4ef41de
VermilionAlex <90432346+VermilionAlex@users.noreply.github.com>
2023-02-16 16:05:12 -0500

    Add mini-template files
    Includes a mini template with a small amount of pseudo-code and comments

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include "gradebook.h"
+
+int main(int argc, char*argv[]) {
+
+    //read arguments
+
+    //create vectors for each of the categories
+
+    //read an input file
+    //tokenize the file
+        //File
+          //Name
+          //Cat 1 grades
+          //Cat 2 grades
+          //Cat 3 grades
+          // Cat 4 grades
+          //Final Grade
+
+
+    //edit file
+
+
+    // option for copy file or revert?
+    return 0;
+}
