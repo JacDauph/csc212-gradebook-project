9f38908b138791d3694c2a12fbbdd285aa408fb1
VermilionAlex <90432346+VermilionAlex@users.noreply.github.com>
2023-02-16 15:40:59 -0500

    Add mini template
    Includes a template for the .cpp and .h files. Also has some pseudo-code.

---
+++ gradebook.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class grade_Book {
+    private:
+        //float weighted grade
+        //float total grade
+        //float grade labs
+        //float grade assignments
+        //float final exam
+        //float review project
+
+    public:
+        //default constructor
+        //getter for a specific grade group
+        //setter for a specific grade group
+        //getter 2 for getting total grade
+        //setter 2 for setting total grade
+
+
+
+};
+
+
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include "gradebook.h"
+
+int main(int argc, char*argv[]) {
+
+    //read arguments
+    //read an input file
+    //edit file
+
+
+    // option for copy file or revert?
+    return 0;
+}
