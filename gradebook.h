7aed7160693118bc97c4f1d55c7317eb6ac5320e
pmajdalani <90423821+pmajdalani@users.noreply.github.com>
2023-02-21 18:58:53 -0500

    Update gradebook.h

--- gradebook.h
+++ gradebook.h
@@ -14,21 +14,21 @@ class grade_Book {
         //contructor with all vectors
         grade_book(std::vector<int> score_obtainted,std::vector<std::string> type, std::vector<std::string> max_score);
         //Getter methods
-        double getIndividualGrade(string assignment_name);
-        double getCategoryGrade(string assignment_type);
+        double getIndividualGrade(std::string assignment_name);
+        double getCategoryGrade(std::string assignment_type);
         double getCourseGrade(int option);
         //helper methods
-        double getWeightFromType(string assignment_type);
-        double getOnlyCategoryTotal(string assignment_type);
-        int getAssignmentIndex(string assignment_name);
+        double getWeightFromType(std::string assignment_type);
+        double getOnlyCategoryTotal(std::string assignment_type);
+        int getAssignmentIndex(std::string assignment_name);
         //modifiers
-        void changeAssignmentName(std::string p_assignment_name, string p_new_name);
-        void changeAssignmentType(std::string p_assignment_name, string p_new_type);
+        void changeAssignmentName(std::string p_assignment_name, std::string p_new_name);
+        void changeAssignmentType(std::string p_assignment_name, std::string p_new_type);
         void changeAssignmentGrade(std::string p_assignment_name, int p_new_score);
         void changeAssignmentMaxGrade(std::string p_assignment_name, int p_new_max_score);
     
         //method for changing file
-        void addNewGrade(string p_new_name, string p_new_type, int p_new_score, int p_new_max_score);
+        void addNewGrade(std::string p_new_name, std::string p_new_type, int p_new_score, int p_new_max_score);
     
         //saves the changes made to file
         void saveChanges();
